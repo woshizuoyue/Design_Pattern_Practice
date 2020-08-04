175e4679bf26fd149b360433a6416f7baf44562a
Yue Zuo <yuezuo@quickenloans.com>
2020-08-04 16:09:45 -0400

    adding factory pattern

---
+++ Factory/SimplePizzaFactory.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Factory
+{
+    public class SimplePizzaFactory
+    {
+
+        public Pizza CreatePizza(string type)
+        {
+            Pizza pizza = null;
+
+            return pizza;
+        }
+    }
+}
